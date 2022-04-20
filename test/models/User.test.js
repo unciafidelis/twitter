5b094a8669040bb4f594023cc7d6981bcfbc0569
unciafidelis <unciafidelis@gmail.com>
2022-04-20 12:40:01 -0500

    Add: test setters

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -20,4 +20,11 @@ describe("Unit Tests for User class", () => {
         expect(user.getDateCreated).not.toBeUndefined()
         expect(user.getLastUpdated).not.toBeUndefined()
     })
+    test('Add setters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
+    })
 })
