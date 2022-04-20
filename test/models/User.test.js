ca89b953ceec42bd43d7627002cc36df12c216a4
unciafidelis <unciafidelis@gmail.com>
2022-04-20 12:23:53 -0500

    Update: User object, dateCreated and lastUpdated

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,14 +3,14 @@ const User = require('../../app/models/User')
 describe("Unit Tests for User class", () => {
     test('Create an user object', () => {
         //code call to use in app
-        const user = new User(1, "carlogilmar", "Carlo","Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "carlogilmar", "Carlo","Bio") //Requirement 2 change
 
         //code validation req
         expect(user.id).toBe(1)
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).not.toBeUndefined()
-        expect(user.lastUpdated).not.toBeUndefined()
+        expect(user.dateCreated).not.toBeUndefined() //Requirement 2 change
+        expect(user.lastUpdated).not.toBeUndefined() //Requirement 2 change
     })
 })
