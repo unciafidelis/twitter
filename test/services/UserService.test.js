68d60aa5e0cee845d3e727cbab871d363db289ec
unciafidelis <unciafidelis@gmail.com>
2022-04-20 13:03:15 -0500

    Add: UserService.js and UserService.test.js passed test

---
+++ test/services/UserService.test.js
@@ -0,0 +1,11 @@
+const UserService = require('./../../app/services/UserService')
+
+describe("Test for UserService", () => {
+    test("1. Create a new user using the UserService", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo")
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+    })
+})
