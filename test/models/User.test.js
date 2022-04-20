4880b99eee347cf30f953deeef6e9545074a4c5a
unciafidelis <unciafidelis@gmail.com>
2022-04-20 12:34:43 -0500

    Fix: "carlogilmar" to "Bio"

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -16,7 +16,7 @@ describe("Unit Tests for User class", () => {
     test('Add getters', () => {
         const user = new User(1, "carlogilmar", "Carlo", "Bio")
         expect(user.getUsername).toBe("carlogilmar")
-        expect(user.getBio).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
         expect(user.getDateCreated).not.toBeUndefined()
         expect(user.getLastUpdated).not.toBeUndefined()
     })
