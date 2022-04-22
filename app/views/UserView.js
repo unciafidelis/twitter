f6c1bb43cb61c687cf216c7f650300c9e01282cd
unciafidelis <unciafidelis@gmail.com>
2022-04-22 14:34:07 -0500

    Add: Class UserView passed tests

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,8 +1,15 @@
+const UserService = require('../../app/services/UserService')
+
 class UserView{
     static createUser(payload){
         if(payload === null){
-            return { error: 'payload no existe' }
+            return { error: "payload no existe" }
         }
+        else if (!payload.username || !payload.name || !payload.id){
+            return { error: "necesitan tener un valor válido"}
+        }
+        const user = UserService.create(payload.id, payload.username, payload.name)
+        return user
     }
 }
 
